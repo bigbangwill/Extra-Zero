1c636493113afbc9f1abbb98b6574c7aef295b33
bigbangwill <mr.hungry.2002@gmail.com>
2024-02-12 23:05:06 +0330

    123

---
+++ Assets/Scripts/Economy/Currency.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public struct IngameCurrency
+{
+    private int currentStack;
+    private int maxStack;
+
+    //public 
+
+    public void SetMaxStack(int maxStack)
+    {
+        this.maxStack = maxStack;
+    }
+
+    public void Reset()
+    {
+        currentStack = 0;
+
+    }
+}
+
+
+
+public class testststs
+{
+
+}
