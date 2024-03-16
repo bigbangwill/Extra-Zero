25d524c0150ecd62c2379371df2dcb72dd250ff7
bigbangwill <mr.hungry.2002@gmail.com>
2024-03-16 22:06:20 +0330

    New Items

--- Assets/Scripts/Items/ItemLibrary.cs
+++ Assets/Scripts/Items/ItemLibrary.cs
@@ -235,6 +235,9 @@ public abstract class BluePrintItem : ItemBehaviour
         return craftTimer;
     }
 
+
+    public abstract CraftedItem CraftedItemReference();
+
     public override bool Equals(object obj)
     {
         return obj is BluePrintItem item &&
@@ -273,6 +276,10 @@ public abstract class BluePrintItem : ItemBehaviour
             materialsList.Add(new MaterialItem.Plastic(1));
             Load();
         }
+        public override CraftedItem CraftedItemReference()
+        {
+            throw new NotImplementedException();
+        }
     }
 
     public class Hoe : BluePrintItem
@@ -286,6 +293,11 @@ public abstract class BluePrintItem : ItemBehaviour
             materialsList.Add(new MaterialItem.AluminumAlloy(1));
             materialsList.Add(new MaterialItem.TitaniumAlloy(1));
             Load();
+        }
+
+        public override CraftedItem CraftedItemReference()
+        {
+            throw new NotImplementedException();
         }
     }
 
@@ -302,6 +314,11 @@ public abstract class BluePrintItem : ItemBehaviour
             materialsList.Add(new MaterialItem.Ceramic(2));
             Load();
         }
+
+        public override CraftedItem CraftedItemReference()
+        {
+            throw new NotImplementedException();
+        }
     }
 
     public class Plant : BluePrintItem
@@ -314,8 +331,377 @@ public abstract class BluePrintItem : ItemBehaviour
             materialsList.Add(new MaterialItem.Ceramic(3));
             Load();
         }
-        
+
+        public override CraftedItem CraftedItemReference()
+        {
+            throw new NotImplementedException();
+        }
     }
+
+    public class DataPadBlueprint : BluePrintItem
+    {
+        public DataPadBlueprint()
+        {
+            specificName = "Data Pad";
+            importTimer = 15; // You may need to set appropriate values for these timers
+            craftTimer = 10;
+            materialsList.Add(new MaterialItem.TitaniumAlloy(2));
+            materialsList.Add(new MaterialItem.Plastic(1));
+            Load();
+        }
+
+        public override CraftedItem CraftedItemReference()
+        {
+            return new CraftedItem.DataPad();
+        }
+    }
+
+    public class SignalBoosterBlueprint : BluePrintItem
+    {
+        public SignalBoosterBlueprint()
+        {
+            specificName = "Signal Booster";
+            importTimer = 15;
+            craftTimer = 10;
+            materialsList.Add(new MaterialItem.StainlessSteel(3));
+            materialsList.Add(new MaterialItem.Ceramic(1));
+            Load();
+        }
+
+        public override CraftedItem CraftedItemReference()
+        {
+            return new CraftedItem.SignalBooster();
+        }
+    }
+
+    public class EnergyCellBlueprint : BluePrintItem
+    {
+        public EnergyCellBlueprint()
+        {
+            specificName = "Energy Cell";
+            importTimer = 15;
+            craftTimer = 10;
+            materialsList.Add(new MaterialItem.AluminumAlloy(2));
+            materialsList.Add(new MaterialItem.TitaniumAlloy(1));
+            Load();
+        }
+
+        public override CraftedItem CraftedItemReference()
+        {
+            return new CraftedItem.EnergyCell();
+        }
+    }
+
+    public class SurvivalKitBlueprint : BluePrintItem
+    {
+        public SurvivalKitBlueprint()
+        {
+            specificName = "Survival Kit";
+            importTimer = 15;
+            craftTimer = 10;
+            materialsList.Add(new MaterialItem.StainlessSteel(1));
+            materialsList.Add(new MaterialItem.Plastic(2));
+            materialsList.Add(new MaterialItem.Ceramic(1));
+            Load();
+        }
+
+        public override CraftedItem CraftedItemReference()
+        {
+            return new CraftedItem.SurvivalKit();
+        }
+    }
+
+    public class RepairDroneBlueprint : BluePrintItem
+    {
+        public RepairDroneBlueprint()
+        {
+            specificName = "Repair Drone";
+            importTimer = 15;
+            craftTimer = 10;
+            materialsList.Add(new MaterialItem.TitaniumAlloy(3));
+            materialsList.Add(new MaterialItem.Plastic(2));
+            Load();
+        }
+
+        public override CraftedItem CraftedItemReference()
+        {
+            return new CraftedItem.RepairDrone();
+        }
+    }
+
+    public class WaterPurifierBlueprint : BluePrintItem
+    {
+        public WaterPurifierBlueprint()
+        {
+            specificName = "Water Purifier";
+            importTimer = 15;
+            craftTimer = 10;
+            materialsList.Add(new MaterialItem.AluminumAlloy(1));
+            materialsList.Add(new MaterialItem.Ceramic(2));
+            Load();
+        }
+
+        public override CraftedItem CraftedItemReference()
+        {
+            return new CraftedItem.WaterPurifier();
+        }
+    }
+
+    public class SolarChargerBlueprint : BluePrintItem
+    {
+        public SolarChargerBlueprint()
+        {
+            specificName = "Solar Charger";
+            importTimer = 15;
+            craftTimer = 10;
+            materialsList.Add(new MaterialItem.StainlessSteel(2));
+            materialsList.Add(new MaterialItem.AluminumAlloy(1));
+            Load();
+        }
+
+        public override CraftedItem CraftedItemReference()
+        {
+            return new CraftedItem.SolarCharger();
+        }
+    }
+
+    public class ThermalBlanketBlueprint : BluePrintItem
+    {
+        public ThermalBlanketBlueprint()
+        {
+            specificName = "Thermal Blanket";
+            importTimer = 15;
+            craftTimer = 10;
+            materialsList.Add(new MaterialItem.TitaniumAlloy(1));
+            materialsList.Add(new MaterialItem.Plastic(2));
+            Load();
+        }
+
+        public override CraftedItem CraftedItemReference()
+        {
+            return new CraftedItem.ThermalBlanket();
+        }
+    }
+
+    public class RadiationDetectorBlueprint : BluePrintItem
+    {
+        public RadiationDetectorBlueprint()
+        {
+            specificName = "Radiation Detector";
+            importTimer = 15;
+            craftTimer = 10;
+            materialsList.Add(new MaterialItem.Ceramic(2));
+            materialsList.Add(new MaterialItem.StainlessSteel(1));
+            Load();
+        }
+
+        public override CraftedItem CraftedItemReference()
+        {
+            return new CraftedItem.RadiationDetector();
+        }
+    }
+
+    public class CommunicationRelayBlueprint : BluePrintItem
+    {
+        public CommunicationRelayBlueprint()
+        {
+            specificName = "Communication Relay";
+            importTimer = 15;
+            craftTimer = 10;
+            materialsList.Add(new MaterialItem.AluminumAlloy(1));
+            materialsList.Add(new MaterialItem.TitaniumAlloy(2));
+            Load();
+        }
+
+        public override CraftedItem CraftedItemReference()
+        {
+            return new CraftedItem.CommunicationRelay();
+        }
+    }
+
+    public class HeatShieldBlueprint : BluePrintItem
+    {
+        public HeatShieldBlueprint()
+        {
+            specificName = "Heat Shield";
+            importTimer = 15;
+            craftTimer = 10;
+            materialsList.Add(new MaterialItem.TitaniumAlloy(3));
+            Load();
+        }
+
+        public override CraftedItem CraftedItemReference()
+        {
+            return new CraftedItem.HeatShield();
+        }
+    }
+
+    public class MemoryCoreBlueprint : BluePrintItem
+    {
+        public MemoryCoreBlueprint()
+        {
+            specificName = "Memory Core";
+            importTimer = 15;
+            craftTimer = 10;
+            materialsList.Add(new MaterialItem.Ceramic(2));
+            Load();
+        }
+
+        public override CraftedItem CraftedItemReference()
+        {
+            return new CraftedItem.MemoryCore();
+        }
+    }
+
+    public class PowerConduitBlueprint : BluePrintItem
+    {
+        public PowerConduitBlueprint()
+        {
+            specificName = "Power Conduit";
+            importTimer = 15;
+            craftTimer = 10;
+            materialsList.Add(new MaterialItem.StainlessSteel(3));
+            Load();
+        }
+
+        public override CraftedItem CraftedItemReference()
+        {
+            return new CraftedItem.PowerConduit();
+        }
+    }
+
+    public class CircuitFrameBlueprint : BluePrintItem
+    {
+        public CircuitFrameBlueprint()
+        {
+            specificName = "Circuit Frame";
+            importTimer = 15;
+            craftTimer = 10;
+            materialsList.Add(new MaterialItem.AluminumAlloy(2));
+            Load();
+        }
+
+        public override CraftedItem CraftedItemReference()
+        {
+            return new CraftedItem.CircuitFrame();
+        }
+    }
+
+    public class InsulationFoamBlueprint : BluePrintItem
+    {
+        public InsulationFoamBlueprint()
+        {
+            specificName = "Insulation Foam";
+            importTimer = 15;
+            craftTimer = 10;
+            materialsList.Add(new MaterialItem.Plastic(3));
+            Load();
+        }
+
+        public override CraftedItem CraftedItemReference()
+        {
+            return new CraftedItem.InsulationFoam();
+        }
+    }
+
+
+    public class AICoreBlueprint : BluePrintItem
+    {
+        public AICoreBlueprint()
+        {
+            specificName = "AI Core";
+            importTimer = 15;
+            craftTimer = 10;
+            materialsList.Add(new MaterialItem.TitaniumAlloy(1));
+            materialsList.Add(new MaterialItem.Ceramic(1));
+            materialsList.Add(new MaterialItem.Plastic(1));
+            Load();
+        }
+
+        public override CraftedItem CraftedItemReference()
+        {
+            return new CraftedItem.AICore();
+        }
+    }
+
+    public class EnvironmentalRegulatorBlueprint : BluePrintItem
+    {
+        public EnvironmentalRegulatorBlueprint()
+        {
+            specificName = "Environmental Regulator";
+            importTimer = 15;
+            craftTimer = 10;
+            materialsList.Add(new MaterialItem.StainlessSteel(1));
+            materialsList.Add(new MaterialItem.AluminumAlloy(1));
+            materialsList.Add(new MaterialItem.Ceramic(1));
+            Load();
+        }
+
+        public override CraftedItem CraftedItemReference()
+        {
+            return new CraftedItem.EnvironmentalRegulator();
+        }
+    }
+
+    public class HapticInterfaceBlueprint : BluePrintItem
+    {
+        public HapticInterfaceBlueprint()
+        {
+            specificName = "Haptic Interface";
+            importTimer = 15;
+            craftTimer = 10;
+            materialsList.Add(new MaterialItem.TitaniumAlloy(1));
+            materialsList.Add(new MaterialItem.StainlessSteel(1));
+            materialsList.Add(new MaterialItem.Plastic(1));
+            Load();
+        }
+
+        public override CraftedItem CraftedItemReference()
+        {
+            return new CraftedItem.HapticInterface();
+        }
+    }
+
+    public class QuantumProcessorBlueprint : BluePrintItem
+    {
+        public QuantumProcessorBlueprint()
+        {
+            specificName = "Quantum Processor";
+            importTimer = 15;
+            craftTimer = 10;
+            materialsList.Add(new MaterialItem.AluminumAlloy(1));
+            materialsList.Add(new MaterialItem.Ceramic(1));
+            materialsList.Add(new MaterialItem.TitaniumAlloy(1));
+            Load();
+        }
+
+        public override CraftedItem CraftedItemReference()
+        {
+            return new CraftedItem.QuantumProcessor();
+        }
+    }
+
+    public class BiometricScannerBlueprint : BluePrintItem
+    {
+        public BiometricScannerBlueprint()
+        {
+            specificName = "Biometric Scanner";
+            importTimer = 15;
+            craftTimer = 10;
+            materialsList.Add(new MaterialItem.StainlessSteel(1));
+            materialsList.Add(new MaterialItem.Plastic(1));
+            materialsList.Add(new MaterialItem.AluminumAlloy(1));
+            Load();
+        }
+
+        public override CraftedItem CraftedItemReference()
+        {
+            return new CraftedItem.BiometricScanner();
+        }
+    }
+
+
+
 }
 
 
@@ -359,6 +745,329 @@ public abstract class CraftedItem : ItemBehaviour
             specificName,
             specificAddress);
     }
+
+    public class DataPad : CraftedItem
+    {
+        public DataPad()
+        {
+            specificName = "DataPad";
+            Load();
+            is_Activeable = false;
+            is_Usable = false;
+        }
+
+        public override void Use()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class SignalBooster : CraftedItem
+    {
+        public SignalBooster()
+        {
+            specificName = "SignalBooster";
+            Load();
+            is_Activeable = false;
+            is_Usable = false;
+        }
+
+        public override void Use()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class EnergyCell : CraftedItem
+    {
+        public EnergyCell()
+        {
+            specificName = "EnergyCell";
+            Load();
+            is_Activeable = false;
+            is_Usable = false;
+        }
+
+        public override void Use()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class SurvivalKit : CraftedItem
+    {
+        public SurvivalKit()
+        {
+            specificName = "SurvivalKit";
+            Load();
+            is_Activeable = false;
+            is_Usable = false;
+        }
+
+        public override void Use()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class RepairDrone : CraftedItem
+    {
+        public RepairDrone()
+        {
+            specificName = "RepairDrone";
+            Load();
+            is_Activeable = false;
+            is_Usable = false;
+        }
+
+        public override void Use()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class WaterPurifier : CraftedItem
+    {
+        public WaterPurifier()
+        {
+            specificName = "WaterPurifier";
+            Load();
+            is_Activeable = false;
+            is_Usable = false;
+        }
+
+        public override void Use()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class SolarCharger : CraftedItem
+    {
+        public SolarCharger()
+        {
+            specificName = "SolarCharger";
+            Load();
+            is_Activeable = false;
+            is_Usable = false;
+        }
+
+        public override void Use()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ThermalBlanket : CraftedItem
+    {
+        public ThermalBlanket()
+        {
+            specificName = "ThermalBlanket";
+            Load();
+            is_Activeable = false;
+            is_Usable = false;
+        }
+
+        public override void Use()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class RadiationDetector : CraftedItem
+    {
+        public RadiationDetector()
+        {
+            specificName = "RadiationDetector";
+            Load();
+            is_Activeable = false;
+            is_Usable = false;
+        }
+
+        public override void Use()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CommunicationRelay : CraftedItem
+    {
+        public CommunicationRelay()
+        {
+            specificName = "CommunicationRelay";
+            Load();
+            is_Activeable = false;
+            is_Usable = false;
+        }
+
+        public override void Use()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    public class HeatShield : CraftedItem
+    {
+        public HeatShield()
+        {
+            specificName = "HeatShield";
+            Load();
+            is_Activeable = false;
+            is_Usable = false;
+        }
+
+        public override void Use()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class MemoryCore : CraftedItem
+    {
+        public MemoryCore()
+        {
+            specificName = "MemoryCore";
+            Load();
+            is_Activeable = false;
+            is_Usable = false;
+        }
+
+        public override void Use()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class PowerConduit : CraftedItem
+    {
+        public PowerConduit()
+        {
+            specificName = "PowerConduit";
+            Load();
+            is_Activeable = false;
+            is_Usable = false;
+        }
+
+        public override void Use()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CircuitFrame : CraftedItem
+    {
+        public CircuitFrame()
+        {
+            specificName = "CircuitFrame";
+            Load();
+            is_Activeable = false;
+            is_Usable = false;
+        }
+
+        public override void Use()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class InsulationFoam : CraftedItem
+    {
+        public InsulationFoam()
+        {
+            specificName = "InsulationFoam";
+            Load();
+            is_Activeable = false;
+            is_Usable = false;
+        }
+
+        public override void Use()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    public class AICore : CraftedItem
+    {
+        public AICore()
+        {
+            specificName = "AICore";
+            Load();
+            is_Activeable = false;
+            is_Usable = false;
+        }
+
+        public override void Use()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class EnvironmentalRegulator : CraftedItem
+    {
+        public EnvironmentalRegulator()
+        {
+            specificName = "EnvironmentalRegulator";
+            Load();
+            is_Activeable = false;
+            is_Usable = false;
+        }
+
+        public override void Use()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class HapticInterface : CraftedItem
+    {
+        public HapticInterface()
+        {
+            specificName = "HapticInterface";
+            Load();
+            is_Activeable = false;
+            is_Usable = false;
+        }
+
+        public override void Use()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class QuantumProcessor : CraftedItem
+    {
+        public QuantumProcessor()
+        {
+            specificName = "QuantumProcessor";
+            Load();
+            is_Activeable = false;
+            is_Usable = false;
+        }
+
+        public override void Use()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class BiometricScanner : CraftedItem
+    {
+        public BiometricScanner()
+        {
+            specificName = "BiometricScanner";
+            Load();
+            is_Activeable = false;
+            is_Usable = false;
+        }
+
+        public override void Use()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
 
 
     public class RepairHammer : CraftedItem
