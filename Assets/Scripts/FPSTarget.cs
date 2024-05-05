e8f1cdb7f5fd39d3f40437ca5c446f3bdf1ed37e
bigbangwill <mr.hungry.2002@gmail.com>
2024-05-05 15:17:46 +0330

    fps

--- Assets/Scripts/FPSTarget.cs
+++ Assets/Scripts/FPSTarget.cs
@@ -5,19 +5,8 @@ using UnityEngine;
 public class FPSTarget : MonoBehaviour
 {
     [SerializeField] private int targetFPS = 0;
-
-    private static FPSTarget instance;
     private void Awake()
     {
-        if (instance == null)
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
         Application.targetFrameRate = targetFPS;
     }
 }
