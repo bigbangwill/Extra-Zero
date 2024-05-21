397d372fc0beda64ceafa029ffbb83783447bedb
bigbangwill <mr.hungry.2002@gmail.com>
2024-05-22 03:10:53 +0330

    Start of Leaderboard

---
+++ Assets/Scripts/LeaderBoard/LeaderboardTest.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Leaderboards;
+using Newtonsoft.Json;
+
+public class LeaderboardTest : MonoBehaviour
+{
+
+    public double score;
+
+    // Create a leaderboard with this ID in the Unity Cloud Dashboard
+    const string LeaderboardId = "Extra_Zero";
+
+    string VersionId { get; set; }
+    int Offset { get; set; }
+    int Limit { get; set; }
+    int RangeLimit { get; set; }
+    List<string> FriendIds { get; set; }
+
+    async void Awake()
+    {
+        await UnityServices.InitializeAsync();
+
+        await SignInAnonymously();
+        await AuthenticationService.Instance.UpdatePlayerNameAsync("BigBang");
+    }
+
+    async Task SignInAnonymously()
+    {
+        AuthenticationService.Instance.SignedIn += () =>
+        {
+            Debug.Log("Signed in as: " + AuthenticationService.Instance.PlayerId);
+        };
+        AuthenticationService.Instance.SignInFailed += s =>
+        {
+            // Take some action here...
+            Debug.Log(s);
+        };
+
+        //await AuthenticationService.Instance.SignInWithGoogleAsync()
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+    }
+
+    public async void AddScore()
+    {
+        var scoreResponse = await LeaderboardsService.Instance.AddPlayerScoreAsync(LeaderboardId, score);
+        Debug.Log(JsonConvert.SerializeObject(scoreResponse));
+    }
+
+    public async void GetScores()
+    {
+        var scoresResponse =
+            await LeaderboardsService.Instance.GetScoresAsync(LeaderboardId);
+        Debug.Log(JsonConvert.SerializeObject(scoresResponse));
+    }
+
+    public async void GetPaginatedScores()
+    {
+        Offset = 10;
+        Limit = 10;
+        var scoresResponse =
+            await LeaderboardsService.Instance.GetScoresAsync(LeaderboardId, new GetScoresOptions { Offset = Offset, Limit = Limit });
+        Debug.Log(JsonConvert.SerializeObject(scoresResponse));
+    }
+
+    public async void GetPlayerScore()
+    {
+        var scoreResponse =
+            await LeaderboardsService.Instance.GetPlayerScoreAsync(LeaderboardId);
+        Debug.Log(JsonConvert.SerializeObject(scoreResponse));
+    }
+
+    public async void GetVersionScores()
+    {
+        var versionScoresResponse =
+            await LeaderboardsService.Instance.GetVersionScoresAsync(LeaderboardId, VersionId);
+        Debug.Log(JsonConvert.SerializeObject(versionScoresResponse));
+    }
+}
